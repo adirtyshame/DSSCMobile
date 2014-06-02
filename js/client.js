ae71a4f62096a9c4e17cb82e0d293d6642f2f444
Thomas Ames <ames.thomas@googlemail.com>
2014-06-02 14:32:46 +0200

    [ENH] Template für Hest angelegt

--- js/client.js
+++ js/client.js
@@ -1,17 +1,30 @@
 (function($) {
+    
       
         var app = $.sammy('#main', function() {
-      
+            
+            this.use('Mustache','mst');
+            
           this.get('#/', function(context) {
             context.log('Yo yo yo');
           });
+          
           this.get('#/about', function(context) {
-            context.log('about');
+              if (true) { 
+                  return this.notFound();
+              }
           });
+          
           this.get('#/contact', function(context) {
-            context.log('contact');
+
+            this.load('http://localhost:4711/class/1D').then(function(items) {
+                    $.each(JSON.parse(items), function (i, heat) {
+                        context.render('templates/heat.mst', {driverNo: heat.driverNo, heatNo: heat.heatNo, timeTotal: heat.timeTotal}).appendTo(context.$element());
+                    });
+            });
+            
+            
           });
-      
         });
       
         $(function() {
